dee929c9a19ca035a0b662580b3b4b95b380d8d0
Lionel DURAND <lioneldurand@gmail.com>
2023-04-14 23:30:04 +0200

    assert mail content, home made spy

--- src/test/java/com/kata/service/GreetingServiceTest.java
+++ src/test/java/com/kata/service/GreetingServiceTest.java
@@ -8,18 +8,37 @@ import org.junit.jupiter.api.Test;
 
 class GreetingServiceTest {
 
+    class MailerSpy extends Mailer {
+        public String greetingValue = "";
+
+        @Override
+        public void send(String greeting) {
+            greetingValue = greeting;
+        }
+    }
+
     @Test
     void should_send_greeting_when_birthay_is_today() {
+        // given
         CustomerRespository customerRespositoryStub = createCustomerRespositoryStub(true);
-        GreetingService greetingService = new GreetingService(customerRespositoryStub, new Mailer());
+        MailerSpy mailerSpy = new MailerSpy();
+        GreetingService greetingService = new GreetingService(customerRespositoryStub, mailerSpy);
+        // when
         greetingService.greeting("World");
+        // then
+        assertEquals("Happy birthday World!", mailerSpy.greetingValue);
     }
 
     @Test
     void should_send_greeting_when_birthay_is_not_today() {
+        // given
         CustomerRespository customerRespositoryStub = createCustomerRespositoryStub(false);
-        GreetingService greetingService = new GreetingService(customerRespositoryStub, new Mailer());
+        MailerSpy mailerSpy = new MailerSpy();
+        GreetingService greetingService = new GreetingService(customerRespositoryStub, mailerSpy);
+        // when
         greetingService.greeting("World");
+        // then
+        assertEquals("Good morning World.", mailerSpy.greetingValue);
     }
 
     private static CustomerRespository createCustomerRespositoryStub(boolean isBirthdayToday) {
